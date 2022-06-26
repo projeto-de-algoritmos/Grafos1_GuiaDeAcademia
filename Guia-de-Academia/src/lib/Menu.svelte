7a80b47e059252c8412329acfdfc8717135c23c1
V100k <vitao.hugo@hotmail.com>
2022-06-26 04:23:40 -0300

    interface inicial

--- Guia-de-Academia/src/lib/Menu.svelte
+++ Guia-de-Academia/src/lib/Menu.svelte
@@ -1,7 +1,169 @@
 <script>
+    import { exercicios_selecionados } from '../store';
+    import { abdominal } from '../store'
+    import { afundo_barra } from '../store'
+    import { agachamento_barra } from '../store'
+    import { alongamento } from '../store'
+    import { barra_fixa } from '../store'
+    import { bicicleta } from '../store'
+    import { corrida } from '../store'
+    import { desenvolvimento_halteres_ombro } from '../store'
+    import { desenvolvimento_ombros_barra } from '../store'
+    import { elevacao_frontal_barra } from '../store'
+    import { elevacao_lateral_halteres } from '../store'
+    import { elevacao_panturrilha } from '../store'
+    import { elevacao_pelvica } from '../store'
+    import { flexao_bracos } from '../store'
+    import { leg_45 } from '../store'
+    import { levantamento_halteres_biceps } from '../store'
+    import { levantamento_terra } from '../store'
+    import { pular_corda } from '../store'
+    import { puxada_alta } from '../store'
+    import { puxada_unilateral } from '../store'
+    import { rosca_concentrada } from '../store'
+    import { rosca_invertida_antebraco } from '../store'
+    import { supino_barra } from '../store'
+    import { supino_inclinado_halteres } from '../store'
 
 </script>
 
+<main>
+    <button>
+        Gerar Sequência de Treino
+    </button>
+    <div>
+        <label>
+            <input type="checkbox" value={$abdominal.name} bind:group={$exercicios_selecionados}>
+            {$abdominal.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$afundo_barra.name} bind:group={$exercicios_selecionados}>
+            {$afundo_barra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$agachamento_barra.name} bind:group={$exercicios_selecionados}>
+            {$agachamento_barra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$alongamento.name} bind:group={$exercicios_selecionados}>
+            {$alongamento.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$barra_fixa.name} bind:group={$exercicios_selecionados}>
+            {$barra_fixa.name} 
+        </label>
+        <label>
+            <input type="checkbox" value={$bicicleta.name} bind:group={$exercicios_selecionados}>
+            {$bicicleta.name}
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="checkbox" value={$corrida.name} bind:group={$exercicios_selecionados}>
+            {$corrida.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$desenvolvimento_halteres_ombro.name} bind:group={$exercicios_selecionados}>
+            {$desenvolvimento_halteres_ombro.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$desenvolvimento_ombros_barra.name} bind:group={$exercicios_selecionados}>
+            {$desenvolvimento_ombros_barra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$elevacao_frontal_barra.name} bind:group={$exercicios_selecionados}>
+            {$elevacao_frontal_barra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$elevacao_lateral_halteres.name} bind:group={$exercicios_selecionados}>
+            {$elevacao_lateral_halteres.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$elevacao_panturrilha.name} bind:group={$exercicios_selecionados}>
+            {$elevacao_panturrilha.name}
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="checkbox" value={$elevacao_pelvica.name} bind:group={$exercicios_selecionados}>
+            {$elevacao_pelvica.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$flexao_bracos.name} bind:group={$exercicios_selecionados}>
+            {$flexao_bracos.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$leg_45.name} bind:group={$exercicios_selecionados}>
+            {$leg_45.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$levantamento_halteres_biceps.name} bind:group={$exercicios_selecionados}>
+            {$levantamento_halteres_biceps.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$levantamento_terra.name} bind:group={$exercicios_selecionados}>
+            {$levantamento_terra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$pular_corda.name} bind:group={$exercicios_selecionados}>
+            {$pular_corda.name}
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="checkbox" value={$puxada_alta.name} bind:group={$exercicios_selecionados}>
+            {$puxada_alta.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$puxada_unilateral.name} bind:group={$exercicios_selecionados}>
+            {$puxada_unilateral.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$rosca_concentrada.name} bind:group={$exercicios_selecionados}>
+            {$rosca_concentrada.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$rosca_invertida_antebraco.name} bind:group={$exercicios_selecionados}>
+            {$rosca_invertida_antebraco.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$supino_barra.name} bind:group={$exercicios_selecionados}>
+            {$supino_barra.name}
+        </label>
+        <label>
+            <input type="checkbox" value={$supino_inclinado_halteres.name} bind:group={$exercicios_selecionados}>
+            {$supino_inclinado_halteres.name}
+        </label>
+    </div>
+</main>
+
 <style>
+    main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    }
+
+    button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: #ff3e00;
+    background-color: rgba(255, 62, 0, 0.1);
+    border-radius: 2em;
+    border: 2px solid rgba(255, 62, 0, 0);
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+    }
+
+    button:focus {
+      border: 2px solid #ff3e00;
+    }
+
+    button:active {
+      background-color: rgba(255, 62, 0, 0.2);
+    }
 
 </style>
---
+++ Guia-de-Academia/src/store.js
@@ -0,0 +1,101 @@
+import { writable } from "svelte/store";
+
+export const exercicios_selecionados = writable([])
+
+export const abdominal = writable({
+    name: 'Abdominal',
+    icon: './assets/icons/abdominal.png'
+})
+export const afundo_barra = writable({
+    name: 'Afuno de Barra',
+    icon: "./assets/icons/afundo_barra.png"
+})
+export const agachamento_barra = writable({
+    name: 'Agachamento de Barra',
+    icon: "./assets/icons/agachamento_barra.png"
+})
+export const alongamento = writable({
+    name: 'Alongamento',
+    icon: "./assets/icons/alongamento.png"
+})
+export const barra_fixa = writable({
+    name: 'Barra Fixa',
+    icon: "./assets/icons/barra_fixa.png"
+})
+export const bicicleta = writable({
+    name: 'Bicileta',
+    icon: "./assets/icons/bicicleta.png"
+})
+export const corrida = writable({
+    name: 'Corrida',
+    icon: "./assets/icons/corrida.png"
+})
+export const desenvolvimento_halteres_ombro = writable({
+    name: 'Desenvolvimento com Halteres para Ombro',
+    icon: "./assets/icons/desenvolvimento_halteres_ombro.png"
+})
+export const desenvolvimento_ombros_barra = writable({
+
+    name: 'Desenvolvimento de Ombros com Barra',
+    icon: "./assets/icons/desenvolvimento_ombros_barra.png"
+})
+export const elevacao_frontal_barra = writable({
+    name: 'Elevação Frontal com Barra',
+    icon: "./assets/icons/elevacao_frontal_barra.png"
+})
+export const elevacao_lateral_halteres = writable({
+    name: 'Elevação Lateral com Halteres',
+    icon: "./assets/icons/elevacao_lateral_halteres.png"
+})
+export const elevacao_panturrilha = writable({
+    name: 'Elevação de Panturrilha',
+    icon: "./assets/icons/elevacao_panturrilha.png"
+})
+export const elevacao_pelvica = writable({
+    name: 'Elevação Pélvica',
+    icon: "./assets/icons/elevacao_pelvica.png"
+})
+export const flexao_bracos = writable({
+    name: 'Flexão de Braços',
+    icon: "./assets/icons/flexao_bracos.png"
+})
+export const leg_45 = writable({
+    name: 'Leg 45°',
+    icon: "./assets/icons/leg_45.png"
+})
+export const  levantamento_halteres_biceps = writable({
+    name: 'Levantamento com Halteres para Bíceps',
+    icon: "./assets/icons/.png"
+})
+export const levantamento_terra = writable({
+    name: 'Levantamento Terra',
+    icon: "./assets/icons/levantamento_terra.png"
+})
+export const pular_corda = writable({
+    name: 'Pular Corda',
+    icon: "./assets/icons/pular_corda.png"
+})
+export const puxada_alta = writable({
+    name: 'Puxada Alta',
+    icon: "./assets/icons/puxada_alta.png"
+})
+export const puxada_unilateral= writable({
+    name: 'Puxada Unilateral',
+    icon: "./assets/icons/puxada_unilateral.png"
+})
+export const rosca_concentrada = writable({
+    name: 'Rosca Concentrada',
+    icon: "./assets/icons/rosca_concentrada.png"
+})
+export const rosca_invertida_antebraco = writable({
+    name: 'Rosca Invertida com Antebraço',
+    icon: "./assets/icons/rosca_invertida_antebraco.png"
+})
+export const  supino_barra = writable({
+    name: 'Supino na Barra',
+    icon: "./assets/icons/supino_barra.png"
+})
+export const supino_inclinado_halteres = writable({
+    name: 'Supino Inclinado com Halteres',
+    icon: "./assets/icons/supino_inclinado_halteres.png"
+})
