dac687e08c8d448035c5352f621ee9dfb12c1b9f
V100k <vitao.hugo@hotmail.com>
2022-06-27 17:30:56 -0300

    correção de alertas

--- Guia-de-Academia/src/store.js
+++ Guia-de-Academia/src/store.js
@@ -5,98 +5,98 @@ export let exercicios_selecionados = writable([])
 
 export const abdominal = writable({
     name: 'Abdominal',
-    icon: "../public/icons/abdominal.png"
+    icon: "../icons/abdominal.png"
 })
 export const afundo_barra = writable({
     name: 'Afundo de Barra',
-    icon: "../public/icons/afundo_barra.png"
+    icon: "../icons/afundo_barra.png"
 })
 export const agachamento_barra = writable({
     name: 'Agachamento de Barra',
-    icon: "../public/icons/agachamento_barra.png"
+    icon: "../icons/agachamento_barra.png"
 })
 export const alongamento = writable({
     name: 'Alongamento',
-    icon: "../public/icons/alongamento.png"
+    icon: "../icons/alongamento.png"
 })
 export const barra_fixa = writable({
     name: 'Barra Fixa',
-    icon: "../public/icons/barra_fixa.png"
+    icon: "../icons/barra_fixa.png"
 })
 export const bicicleta = writable({
     name: 'Bicileta',
-    icon: "../public/icons/bicicleta.png"
+    icon: "../icons/bicicleta.png"
 })
 export const corrida = writable({
     name: 'Corrida',
-    icon: "../public/icons/corrida.png"
+    icon: "../icons/corrida.png"
 })
 export const desenvolvimento_halteres_ombro = writable({
     name: 'Desenvolvimento com Halteres para Ombro',
-    icon: "../public/icons/desenvolvimento_halteres_ombro.png"
+    icon: "../icons/desenvolvimento_halteres_ombro.png"
 })
 export const desenvolvimento_ombros_barra = writable({
 
     name: 'Desenvolvimento de Ombros com Barra',
-    icon: "../public/icons/desenvolvimento_ombros_barra.png"
+    icon: "../icons/desenvolvimento_ombros_barra.png"
 })
 export const elevacao_frontal_barra = writable({
     name: 'Elevação Frontal com Barra',
-    icon: "../public/icons/elevacao_frontal_barra.png"
+    icon: "../icons/elevacao_frontal_barra.png"
 })
 export const elevacao_lateral_halteres = writable({
     name: 'Elevação Lateral com Halteres',
-    icon: "../public/icons/elevacao_lateral_halteres.png"
+    icon: "../icons/elevacao_lateral_halteres.png"
 })
 export const elevacao_panturrilha = writable({
     name: 'Elevação de Panturrilha',
-    icon: "../public/icons/elevacao_panturrilha.png"
+    icon: "../icons/elevacao_panturrilha.png"
 })
 export const elevacao_pelvica = writable({
     name: 'Elevação Pélvica',
-    icon: "../public/icons/elevacao_pelvica.png"
+    icon: "../icons/elevacao_pelvica.png"
 })
 export const flexao_bracos = writable({
     name: 'Flexão de Braços',
-    icon: "../public/icons/flexao_bracos.png"
+    icon: "../icons/flexao_bracos.png"
 })
 export const leg_45 = writable({
     name: 'Leg 45°',
-    icon: "../public/icons/leg_45.png"
+    icon: "../icons/leg_45.png"
 })
 export const levantamento_halteres_biceps = writable({
     name: 'Levantamento com Halteres para Bíceps',
-    icon: "../public/icons/levantamento_halteres_biceps.png"
+    icon: "../icons/levantamento_halteres_biceps.png"
 })
 export const levantamento_terra = writable({
     name: 'Levantamento Terra',
-    icon: "../public/icons/levantamento_terra.png"
+    icon: "../icons/levantamento_terra.png"
 })
 export const pular_corda = writable({
     name: 'Pular Corda',
-    icon: "../public/icons/pular_corda.png"
+    icon: "../icons/pular_corda.png"
 })
 export const puxada_alta = writable({
     name: 'Puxada Alta',
-    icon: "../public/icons/puxada_alta.png"
+    icon: "../icons/puxada_alta.png"
 })
 export const puxada_unilateral= writable({
     name: 'Puxada Unilateral',
-    icon: "../public/icons/puxada_unilateral.png"
+    icon: "../icons/puxada_unilateral.png"
 })
 export const rosca_concentrada = writable({
     name: 'Rosca Concentrada',
-    icon: "../public/icons/rosca_concentrada.png"
+    icon: "../icons/rosca_concentrada.png"
 })
 export const rosca_invertida_antebraco = writable({
     name: 'Rosca Invertida com Antebraço',
-    icon: "../public/icons/rosca_invertida_antebraco.png"
+    icon: "../icons/rosca_invertida_antebraco.png"
 })
 export const  supino_barra = writable({
     name: 'Supino na Barra',
-    icon: "../public/icons/supino_barra.png"
+    icon: "../icons/supino_barra.png"
 })
 export const supino_inclinado_halteres = writable({
     name: 'Supino Inclinado com Halteres',
-    icon: "../public/icons/supino_inclinado_halteres.png"
+    icon: "../icons/supino_inclinado_halteres.png"
 })
